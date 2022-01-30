632d9b8840d21c2f186dcd8d4b9a103df7073885
dtscott9 <dtscott9@byui.edu>
2022-01-29 19:30:02 -0700

    implemented hamburger menu

--- chamber/js/basic.js
+++ chamber/js/basic.js
@@ -8,3 +8,9 @@ document.getElementById("date").textContent = currDate;
 let mod = document.lastModified
 
 document.getElementById("modified").textContent = mod;
+
+const menuButton = document.querySelector(".ham");
+const navi = document.querySelector("ul");
+
+menuButton.addEventListener("click", () =>
+{navi.classList.toggle("responsive")}, false);
