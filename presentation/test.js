704809d3065a25ba0bafd77f2bd483109e01c865
dtscott9 <dtscott9@byui.edu>
2022-02-08 23:56:29 -0700

    practice

---
+++ presentation/test.js
@@ -0,0 +1,8 @@
+const b = document.querySelector(".demonstration2");
+const btn = document.querySelector("button");
+
+function ab(){
+    b.style.display = "block";
+}
+
+btn.addEventListener("click", ab);
