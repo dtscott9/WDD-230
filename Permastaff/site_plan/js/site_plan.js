e550734129df6749cdc4fa5a0d745fa6988fcf53
dtscott9 <dtscott9@byui.edu>
2022-03-26 20:58:31 -0600

    permastaff

---
+++ Permastaff/site_plan/js/site_plan.js
@@ -0,0 +1,5 @@
+const menuButton = document.querySelector(".ham");
+const navi = document.querySelector(".links");
+
+menuButton.addEventListener("click", () =>
+{navi.classList.toggle("responsive")}, false);
