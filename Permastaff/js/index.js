a12178c63fb49b45c07dd0a98454a6485944b1e5
dtscott9 <dtscott9@byui.edu>
2022-04-06 23:46:08 -0600

    fixed several errors

--- Permastaff/js/index.js
+++ Permastaff/js/index.js
@@ -1,30 +1,52 @@
 const menuButton = document.querySelector(".ham");
 const navi = document.querySelector(".links");
 
-menuButton.addEventListener("click", () =>
-{navi.classList.toggle("responsive")}, false);
+menuButton.addEventListener(
+  "click",
+  () => {
+    navi.classList.toggle("responsive");
+  },
+  false
+);
 
 const date = new Date();
 
-
-let mod = document.lastModified
+let mod = document.lastModified;
 
 document.getElementById("modified").textContent = mod;
 
-let lazyImages = Array.from(document.querySelectorAll(".ourSolutions img, .pageImg"));
+let lazyImages = Array.from(
+  document.querySelectorAll(".ourSolutions img, .pageImg")
+);
 
 if ("IntersectionObserver" in window) {
-    const imageObsever = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                const lazy = entry.target;
+  const imageObsever = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const lazy = entry.target;
 
-                lazy.src = lazy.dataset.src;
-                imageObsever.unobserve(lazy);
-            }
-        });
+        lazy.src = lazy.dataset.src;
+        imageObsever.unobserve(lazy);
+      }
     });
+  });
 
-    lazyImages.forEach(img => imageObsever.observe(img));
-
+  lazyImages.forEach((img) => imageObsever.observe(img));
 }
+
+const loginButton = document.querySelector(".login");
+
+function buttonLink() {
+  location.href =
+    "https://cyma.permastaff.com/permastaff/(S(2bbni00sgkxf03ouchvscbpp))/default.aspx";
+}
+
+loginButton.addEventListener("click", buttonLink);
+
+const contactButton = document.querySelector(".bannerButton");
+
+function bannerLink() {
+    location.href = "contact.html";
+}
+
+contactButton.addEventListener("click", bannerLink);
