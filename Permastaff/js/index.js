0943a0479761690544400cc5e72337ca226cbeea
dtscott9 <dtscott9@byui.edu>
2022-04-06 23:14:20 -0600

    added lazy loading

--- Permastaff/js/index.js
+++ Permastaff/js/index.js
@@ -10,3 +10,21 @@ const date = new Date();
 let mod = document.lastModified
 
 document.getElementById("modified").textContent = mod;
+
+let lazyImages = Array.from(document.querySelectorAll(".ourSolutions img, .pageImg"));
+
+if ("IntersectionObserver" in window) {
+    const imageObsever = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const lazy = entry.target;
+
+                lazy.src = lazy.dataset.src;
+                imageObsever.unobserve(lazy);
+            }
+        });
+    });
+
+    lazyImages.forEach(img => imageObsever.observe(img));
+
+}
